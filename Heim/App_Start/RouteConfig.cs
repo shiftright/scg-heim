bec35988c88bbe23ce4ab4fa215022eed34f885a
poklag <pokpag@gmail.com>
2013-11-19 20:08:13 +0700

    Updated user menu

--- Heim/App_Start/RouteConfig.cs
+++ Heim/App_Start/RouteConfig.cs
@@ -10,16 +10,16 @@ namespace ShiftRight {
 		public static void RegisterRoutes(RouteCollection routes) {
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			routes.MapRoute(
-				name: "Projects",
-				url: "Projects",
-				defaults: new { controller = "Projects", action = "Home" }
-			);
+			//routes.MapRoute(
+			//	name: "Projects",
+			//	url: "{controller}",
+			//	defaults: new { controller = "Projects", action = "Home" }
+			//);
 
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Projects", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Projects", action = "Home", id = UrlParameter.Optional }
 			);
 		}
 	}

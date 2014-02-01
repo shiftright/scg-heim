7ef9c463860b7ae8a408119aa7ef171a7ce096b8
Pisin Rerngniransathit <pisin@shiftright.co.th>
2014-02-01 22:38:36 +0700

    add default user

--- Heim/Migrations/Configuration.cs
+++ Heim/Migrations/Configuration.cs
@@ -3,6 +3,8 @@ namespace ShiftRight.Heim.Migrations {
 	using System.Data.Entity;
 	using System.Data.Entity.Migrations;
 	using System.Linq;
+	using System.Web.Security;
+	using WebMatrix.WebData;
 
 	internal sealed class Configuration : DbMigrationsConfiguration<ShiftRight.Heim.Models.HeimContext> {
 		public Configuration() {
@@ -22,6 +24,22 @@ namespace ShiftRight.Heim.Migrations {
 			//      new Person { FullName = "Rowan Miller" }
 			//    );
 			//
+
+			WebSecurity.InitializeDatabaseConnection("DefaultConnection", "Users", "ID", "UserName", autoCreateTables: true);
+
+			if (!Roles.RoleExists("admin")) {
+				Roles.CreateRole("admin");
+			}
+			if (!WebSecurity.UserExists("admin")) {
+				WebSecurity.CreateUserAndAccount("admin", "heim1234", new {
+					Email = "pisin.r@gmail.com"
+				});
+			}
+			if(!Roles.GetRolesForUser("admin").Contains("admin")) {
+				Roles.AddUserToRole("admin", "admin");
+			}
+
+
 		}
 	}
 }
